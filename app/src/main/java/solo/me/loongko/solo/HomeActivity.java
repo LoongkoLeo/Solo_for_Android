36298cc623dfebeeb2638d647f5710be95bdc602
LoongkoLeo <zdzz1091@gmail.com>
2018-10-12 11:00:38 +0800

    Solo_for_Android
    A Android app for Solo, powered by B3log_Solo, coded by BanXian Liu.

--- app/src/main/java/solo/me/loongko/solo/HomeActivity.java
+++ app/src/main/java/solo/me/loongko/solo/HomeActivity.java
@@ -2,12 +2,20 @@ package solo.me.loongko.solo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView item_home_recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void initView(){
+        item_home_recyclerview=findViewById(R.id.item_home_recyclerview)  ;
+
     }
 }
